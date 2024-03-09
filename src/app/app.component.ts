2759d7b68902bb7600cebee0920e221306a9ac13
Louis Poirier <contact@louispoirier.io>
2024-03-09 01:19:37 +0100

    refactor: remove obsolete module import.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MatToolbar} from "@angular/material/toolbar";
-import {HttpClientModule} from "@angular/common/http";
 import {IconService} from "./services/icon.service";
 
 @Component({
   selector: 'fig-app',
   standalone: true,
-  imports: [RouterOutlet, MatToolbar, HttpClientModule],
+  imports: [RouterOutlet, MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
