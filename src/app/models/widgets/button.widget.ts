fb42fac44b4f413e79516ff62efe280128b24fc5
Louis Poirier <contact@louispoirier.io>
2024-03-14 20:04:30 +0100

    feat: add small and arrow features on Button widget.

--- src/app/models/widgets/button.widget.ts
+++ src/app/models/widgets/button.widget.ts
@@ -1,12 +1,24 @@
 import {FIGWidgetType} from "./widget";
 import {FIGWithTooltip} from "./with-tooltip.widget";
 
+export enum FIGArrowDirection {
+  left,
+  right,
+  up,
+  down,
+  none = -1
+}
+
 export class FIGButtonWidget extends FIGWithTooltip {
   text: string;
+  isSmall: boolean;
+  arrow: FIGArrowDirection;
 
-  constructor(text: string = 'Button', tooltip?: string) {
+  constructor(text: string = 'Button', isSmall: boolean = false, arrow: FIGArrowDirection = FIGArrowDirection.none, tooltip?: string) {
     super(FIGWidgetType.button, true);
     this.text = text;
+    this.isSmall = isSmall;
+    this.arrow = arrow;
     this.tooltip = tooltip;
   }
 
@@ -15,7 +27,13 @@ export class FIGButtonWidget extends FIGWithTooltip {
   }
 
   public override draw(): void {
-    ImGui.Button(this.text);
+    if (this.isSmall) {
+      ImGui.SmallButton(this.text);
+    } else if (this.arrow !== FIGArrowDirection.none) {
+      ImGui.ArrowButton(this.text, this.arrow);
+    } else {
+      ImGui.Button(this.text);
+    }
     super.draw();
   }
 }
--- src/app/pages/editor/properties/button-properties/button-properties.component.ts
+++ src/app/pages/editor/properties/button-properties/button-properties.component.ts
@@ -3,9 +3,11 @@ import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {FIGWidget} from "../../../../models/widgets/widget";
-import {FIGButtonWidget} from "../../../../models/widgets/button.widget";
+import {FIGArrowDirection, FIGButtonWidget} from "../../../../models/widgets/button.widget";
 import {MatIcon} from "@angular/material/icon";
 import {AbstractPropertiesComponent} from "../abstract-properties.component";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {MatOption, MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'fig-button-properties',
@@ -14,7 +16,10 @@ import {AbstractPropertiesComponent} from "../abstract-properties.component";
     MatIcon,
     MatInput,
     MatLabel,
+    MatSelect,
+    MatOption,
     MatFormField,
+    MatSlideToggle,
     ReactiveFormsModule
   ],
   templateUrl: './button-properties.component.html',
@@ -28,12 +33,18 @@ export class ButtonPropertiesComponent extends AbstractPropertiesComponent<FIGBu
   override form: FormGroup = new FormGroup<any>({
     text: new FormControl<string>(''),
     tooltip: new FormControl<string | null>(null),
+    isSmall: new FormControl<boolean>(false),
+    arrow: new FormControl<FIGArrowDirection>(FIGArrowDirection.none),
   });
+
+  protected readonly FIGArrowDirection = FIGArrowDirection;
 
   constructor(dr: DestroyRef) {
     super(dr);
     this.listenProperty('text').subscribe(this.onTextChanged.bind(this));
     this.listenProperty('tooltip').subscribe(this.onTooltipChanged.bind(this));
+    this.listenProperty('isSmall').subscribe(this.onIsSmallChanged.bind(this));
+    this.listenProperty('arrow').subscribe(this.onArrowChanged.bind(this));
   }
 
   protected override updateForm() {
@@ -42,6 +53,8 @@ export class ButtonPropertiesComponent extends AbstractPropertiesComponent<FIGBu
     }
     this.setProperty('text', this.widget.text);
     this.setProperty('tooltip', this.widget.tooltip ?? null);
+    this.setProperty('isSmall', this.widget.isSmall);
+    this.setProperty('arrow', this.widget.arrow);
   }
 
   private onTextChanged(value: string): void {
@@ -54,6 +67,24 @@ export class ButtonPropertiesComponent extends AbstractPropertiesComponent<FIGBu
       value = null;
     }
     this.widget!.tooltip = value ?? undefined;
+    this.update.emit();
+  }
+
+  private onIsSmallChanged(value: boolean): void {
+    this.widget!.isSmall = value;
+    if (value && this.widget!.arrow !== FIGArrowDirection.none) {
+      this.setProperty('arrow', FIGArrowDirection.none);
+      this.widget!.arrow = FIGArrowDirection.none;
+    }
+    this.update.emit();
+  }
+
+  private onArrowChanged(value: FIGArrowDirection): void {
+    this.widget!.arrow = value;
+    if (value && this.widget!.isSmall) {
+      this.setProperty('isSmall', false);
+      this.widget!.isSmall = false;
+    }
     this.update.emit();
   }
 
