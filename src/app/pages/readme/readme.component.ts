e49739a9441d5bde789bd89e6d997eb46d6b890c
Louis Poirier <contact@louispoirier.io>
2024-05-29 23:04:46 +0200

    feat: add page <fig-readme> with slides.
    Describe features along with animated GIFs.
    WIP, need more slides.

--- src/app/pages/readme/readme.component.ts
+++ src/app/pages/readme/readme.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatFabButton} from "@angular/material/button";
 
 @Component({
   selector: 'fig-readme',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatFabButton
+  ],
   templateUrl: './readme.component.html',
   styleUrl: './readme.component.css'
 })
