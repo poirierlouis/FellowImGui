4e634e6c8a519cc3864cb4ff9261f2dc4019a946
Louis Poirier <contact@louispoirier.io>
2024-09-09 23:04:49 +0200

    test: add units for AppComponent.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,52 @@
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {IconService} from "./services/icon.service";
+import {MatIconTestingModule} from "@angular/material/icon/testing";
+import {MatButtonHarness} from "@angular/material/button/testing";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {getHarness} from "../test/helpers.test";
+
+jest.mock('./services/icon.service');
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        NoopAnimationsModule,
+        MatIconTestingModule,
+
+        AppComponent
+      ],
+      providers: [
+        IconService
+      ]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should load icons', () => {
+    const iconService = TestBed.inject(IconService);
+
+    expect(iconService.load).toHaveBeenCalled();
+  });
+
+  it('should open GitHub in a new tab', async () => {
+    // GIVEN
+    const $button: MatButtonHarness = await getHarness(fixture, MatButtonHarness.with({selector: '[data-testid=btn-github]'}));
+
+    // WHEN
+    await $button.click();
+
+    // THEN
+    expect(window.open).toHaveBeenCalledWith('https://github.com/poirierlouis/FellowImGui', '_blank');
   });
 });
